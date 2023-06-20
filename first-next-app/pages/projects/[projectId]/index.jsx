2bb0de4d95ff543d9ccce2a1bcf75e19eec418f6
Magesh Kuppan <tkmagesh77@gmail.com>
2023-06-20 12:29:54 +0530

    more SSG

--- first-next-app/pages/projects/[projectId]/index.jsx
+++ first-next-app/pages/projects/[projectId]/index.jsx
@@ -3,21 +3,42 @@ import axios from "axios";
 
 export default function ProjectInfoPage(props){
     const { project } = props;
+    console.log(props);
+
     return (
         <>
-            <h3>{project.name} - [{project.id}]</h3>
+            <h3>{project?.name} - [{project?.id}]</h3>
             <hr/>
-            <div>{project.desc}</div>
+            <div>{project?.desc}</div>
         </>
     )
 }
 
-export async function getServerSideProps({query}){
-    const { projectId } = query;
+export async function getStaticProps({params}){
+    const { projectId } = params;
     const response = await axios.get(`http://localhost:3030/projects/${projectId}`)
     return {
         props : {
             project : response.data
         }
+    }
+}
+
+export async function getStaticPaths(){
+    const paths = [
+        {
+            params : {
+                projectId : "1"
+            }
+        },
+        {
+            params : {
+                projectId : "2"
+            }
+        }
+    ]
+    return {
+        paths : paths,
+        fallback : true
     }
 }
