0999edbc98afcd49a18fd522e03f430b6f5fff0a
Magesh Kuppan <tkmagesh77@gmail.com>
2023-06-20 11:58:42 +0530

    display project info

--- first-next-app/pages/projects/[projectId]/index.jsx
+++ first-next-app/pages/projects/[projectId]/index.jsx
@@ -1,13 +1,23 @@
 import { useRouter } from "next/router";
+import axios from "axios";
 
-export default function ProjectInfoPage(){
-    const router = useRouter()
-    const { projectId } = router.query
+export default function ProjectInfoPage(props){
+    const { project } = props;
     return (
         <>
-            <h3>Project - [{projectId}]</h3>
+            <h3>{project.name} - [{project.id}]</h3>
             <hr/>
-            <div>Information about [{projectId}] will be displayed here...</div>
+            <div>{project.desc}</div>
         </>
     )
+}
+
+export async function getServerSideProps({query}){
+    const { projectId } = query;
+    const response = await axios.get(`http://localhost:3030/projects/${projectId}`)
+    return {
+        props : {
+            project : response.data
+        }
+    }
 }
--- first-next-app/pages/projects/index.jsx
+++ first-next-app/pages/projects/index.jsx
@@ -51,6 +51,7 @@ export async function getStaticProps(){
     return {
         props : {
             list : projects
-        }
+        },
+        revalidate : 10
     }
 } 
